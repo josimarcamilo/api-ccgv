40924fcec5eebdc82b1ecff5c97726b8b83f014b
Josimar Camilo <josimarifmg@gmail.com>
2025-05-04 22:54:48 -0300

    criando configuracao na categoria

---
+++ models/categoria.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID          uint           `gorm:"primarykey" json:"id"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	TeamID      uint           `gorm:"index" json:"team_id"` // FK para Time
+	Type        int8           `gorm:"null" json:"type"`
+	Name        string         `gorm:"size:255;not null" form:"name" json:"name"`
+	UseMap      bool           `gorm:"null;default:false" form:"use_map" json:"use_map"`
+	TipoRepasse int8           `gorm:"null;default:0" form:"tipo_repasse" json:"tipo_repasse"` // 0 - não repassa, 1 - 10%, 2 - 2,5%
+}
--- models/models.go
+++ models/models.go
@@ -6,17 +6,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type Category struct {
-	ID        uint           `gorm:"primarykey" json:"id"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
-	TeamID    uint           `gorm:"index" json:"team_id"` // FK para Time
-	Type      int8           `gorm:"null" json:"type"`
-	Name      string         `gorm:"size:255;not null" form:"name" json:"name"`
-	UseMap    bool           `gorm:"null;default:false" form:"use_map" json:"use_map"`
-}
-
 type Account struct {
 	ID          uint           `gorm:"primarykey" json:"id"`
 	CreatedAt   time.Time      `json:"created_at"`
